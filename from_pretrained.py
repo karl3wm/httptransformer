7d392d7765d3ef858d9953eef297a8b14abb09d2
karl <karl3wm@writeme.com>
2025-05-04 10:48:57 -0400

    some verbose in from_pretrained

--- from_pretrained.py
+++ from_pretrained.py
@@ -4,6 +4,7 @@ import torch
 import nettensors
 
 nettensors_kwparams = dict(trust_sizes=True, mem_usage_frac=0.25, disk_usage_frac=0.95)
+__verbose = False
 
 @classmethod
 def diffusers_ModelMixin_from_pretrained(cls, name, torch_dtype=None, low_cpu_mem_usage=None, device_map=None, subfolder='', max_memory=None, offload_folder=None, offload_state_dict=False, use_safetensors=None):
@@ -19,7 +20,7 @@ def diffusers_ModelMixin_from_pretrained(cls, name, torch_dtype=None, low_cpu_me
     with accelerate.init_empty_weights():
         model = cls.from_config(config, low_cpu_mem_usage=low_cpu_mem_usage)
     #state_dict = nettensors.from_hf_hub(name, lfs_filename='diffusion_pytorch_model.safetensors', **nettensors_kwparams)
-    state_dict = nettensors.from_hf_hub(name, subfolder=subfolder, **nettensors_kwparams)
+    state_dict = dict(nettensors.from_hf_hub(name, subfolder=subfolder, **nettensors_kwparams))
     model._convert_deprecated_attention_blocks(state_dict)
     diffusers.models.modeling_utils.load_model_dict_into_meta(model, state_dict, device='cpu', dtype=torch_dtype, model_name_or_path=name, hf_quantizer=hf_q)
     if hf_q is not None:
@@ -81,14 +82,50 @@ def diffusers_DiffusionPipeline_from_pretrained(cls, name, torch_dtype=None,prov
                 loading_kwparams['low_cpu_mem_usage'] = False
         loaded_sub_model = load_method(name, **loading_kwparams, subfolder=sub_name)
         sub_models[sub_name] = loaded_sub_model
-    return _diffusers_DiffusionPipeline_from_pretrained(cls, name, torch_dtype=torch_dtype,provider=provider,sess_options=sess_options,device_map=device_map,max_memory=max_memory,offload_folder=offload_folder,offload_state_dict=offload_state_dict,from_flax=from_flax,variant=variant,low_cpu_mem_usage=low_cpu_mem_usage,use_safetensors=use_safetensors,**kwparams,**sub_models)
+    if __verbose:
+        import tqdm
+        pbar = None
+        pbar_total = len(sub_models)
+        def wrap_forward(name, model, is_root=False):
+            if is_root:
+                pipe = model
+                import tqdm
+                def pipe_progress(*params, **kwparams):
+                    nonlocal pbar
+                    pbar = tqdm.tqdm(total=pbar_total,desc=name,leave=False,unit='m')
+                    with pbar:
+                        return pipe(*params, **kwparams)
+                pipe_progress.pipe = pipe
+                pipe_progress.sub_models = sub_models
+                return pipe_progress
+            else:
+                if not hasattr(model, 'forward'):
+                    nonlocal pbar_total
+                    pbar_total -= 1
+                    return model
+                wrapped_forward = model.forward
+                def forward(*params, **kwparams):
+                    pbar.desc = name
+                    pbar.update(0)
+                    result = wrapped_forward(*params, **kwparams)
+                    pbar.update()
+                    return result
+                model.forward = forward
+                return model
+        for sub_name, sub_model in list(sub_models.items()):
+            sub_models[name] = wrap_forward(sub_name, sub_model)
+        return wrap_forward(name, _diffusers_DiffusionPipeline_from_pretrained(cls, name, torch_dtype=torch_dtype,provider=provider,sess_options=sess_options,device_map=device_map,max_memory=max_memory,offload_folder=offload_folder,offload_state_dict=offload_state_dict,from_flax=from_flax,variant=variant,low_cpu_mem_usage=low_cpu_mem_usage,use_safetensors=use_safetensors,**kwparams,**sub_models), True)
+    else:
+        return _diffusers_DiffusionPipeline_from_pretrained(cls, name, torch_dtype=torch_dtype,provider=provider,sess_options=sess_options,device_map=device_map,max_memory=max_memory,offload_folder=offload_folder,offload_state_dict=offload_state_dict,from_flax=from_flax,variant=variant,low_cpu_mem_usage=low_cpu_mem_usage,use_safetensors=use_safetensors,**kwparams,**sub_models)
 
 @classmethod
 def stub_from_pretrained(cls, *params, **kwparams):
     raise NotImplementedError(cls.__name__ + '.from_pretrained')
 
 @contextlib.contextmanager
-def nettensors_from_pretrained(**kwparams):
+def nettensors_from_pretrained(verbose=True, **kwparams):
+    global __verbose
+    __verbose_cached = __verbose
     if kwparams:
         global nettensors_kwparams
         nettensors_kwparams = kwparams
@@ -98,14 +135,38 @@ def nettensors_from_pretrained(**kwparams):
             for clsname in diffusers.pipelines.pipeline_utils.LOADABLE_CLASSES[pkgname]:
                 f = globals().get(f'{pkgname}_{clsname}_from_pretrained')
                 if f is None:
-                    if 'Pipeline' in clsname or 'Tokenizer' in clsname or 'Scheduler' in clsname:
+                    if 'Pipeline' in clsname or 'Tokenizer' in clsname or 'Scheduler' in clsname or 'ImageProcess' in clsname:
                         continue
+                    print('STUB:', clsname)
                     f = stub_from_pretrained
                 cls = getattr(pkg, clsname)
                 stashed_from_pretrained[cls] = getattr(cls, 'from_pretrained')
                 setattr(cls, 'from_pretrained', f)
         print(stashed_from_pretrained.keys())
+        if verbose and not __verbose:
+            __Progress.enable_layer_progress()
+        __verbose = verbose
         yield
     finally:
+        __verbose = __verbose_cached
         for cls, stashed in stashed_from_pretrained.items():
             setattr(cls, 'from_pretrained', stashed)
+
+class __Progress:
+    def enable_layer_progress():
+        import tqdm
+        _modulelistiter = torch.nn.ModuleList.__iter__
+        def module_list_iter(self):
+            try:
+                name = modnames[self]
+            except:
+                name = self.__class__.__name__
+            with tqdm.tqdm(
+                _modulelistiter(self),
+                desc=name,
+                total=len(self),
+                unit='lyr',
+                leave=False
+            ) as pbar:
+                yield from pbar
+        torch.nn.ModuleList.__iter__ = module_list_iter
